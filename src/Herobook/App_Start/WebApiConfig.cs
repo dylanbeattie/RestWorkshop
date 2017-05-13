76bae293a0e1f4e8e4ffe1a45f7013195290e5ce
dylanbeattie <dylan@dylanbeattie.net>
2017-05-13 15:03:22 +0100

    Skeleton solution design and MVC/API controllers for new version of REST workshop

---
+++ src/Herobook/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace Herobook {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
+            // Configure support for sending JSON to regular web browser requests.
+            config.Formatters.Add(new BrowserJsonFormatter());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Herobook/Controllers/Api/ProfilesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Herobook.Data;
+
+namespace Herobook.Controllers.Api {
+    public class ProfilesController : ApiController {
+
+        private IDatabase database;
+        public ProfilesController() {
+            database = new DemoDatabase();
+        }
+
+        // GET api/<controller>
+        public object Get() {
+            return (database.ListProfiles());
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value) { }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value) { }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id) { }
+    }
+}
